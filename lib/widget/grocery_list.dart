32fe106372f751889de5956fb0a0be3425e032ca
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-05-01 17:01:10 +0100

    returned the project without the future builder

--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -14,26 +14,33 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-  late Future<List<GroceryItem>> _loadedItems;
-  String? _error;
+  var _isLoading = true;
+  String ? _error;
 
   @override
   void initState() {
     super.initState();
-    _loadedItems = _loadItems();
+    _loadItems();
   }
 
-  Future<List<GroceryItem>> _loadItems() async {
+  void _loadItems() async {
     final url = Uri.https(
         'joeshoppinglistapp-default-rtdb.firebaseio.com', 'shopping-list.json');
 
-    final response = await http.get(url);
-    if (response.statusCode >= 400) {
-      throw Exception('Failed to fecth data. Please try again latter');
+        try{
+          final response = await http.get(url);
+           if(response.statusCode >= 400){
+      setState(() {
+        _error = 'Failed to load items. please try again';
+      });
+      
     }
 
-    if (response.body == 'null') {
-      return [];
+    if(response.body == 'null'){
+      setState(() {
+        _isLoading = false;
+      });
+      return;
     }
 
     final Map<String, dynamic> listData = json.decode(response.body);
@@ -51,7 +58,18 @@ class _GroceryListState extends State<GroceryList> {
           category: category));
     }
 
-    return loadedItems;
+    setState(() {
+      _groceryItems = loadedItems;
+      _isLoading = false;
+    });
+
+        }catch(error){
+             setState(() {
+        _error = 'Something went wrong! please try again';
+      });
+        }
+    
+   
   }
 
   void _addItem() async {
@@ -72,31 +90,78 @@ class _GroceryListState extends State<GroceryList> {
   void _removeItem(GroceryItem item) async {
     final index = _groceryItems.indexOf(item);
 
-    setState(() {
+     setState(() {
       _groceryItems.remove(item);
+    });  
+
+    final url = Uri.https(
+        'joeshoppinglistapp-default-rtdb.firebaseio.com', 'shopping-list/${item.id}.json');
+       final response = await  http.delete(url);
+
+      if(response.statusCode >= 400){
+        //optional you can show error message 
+        setState(() {
+      _groceryItems.insert(index, item);
+      _error = 'Failed to delete item. Please try again latter.';
+
     });
 
-    final url = Uri.https('joeshoppinglistapp-default-rtdb.firebaseio.com',
-        'shopping-list/${item.id}.json');
-    final response = await http.delete(url);
-
-    if (response.statusCode >= 400) {
-      //optional you can show error message
+    Future.delayed(const Duration(seconds: 1), () {
       setState(() {
-        _groceryItems.insert(index, item);
-        _error = 'Failed to delete item. Please try again latter.';
+        _error = null; // Clear error message
       });
 
-      Future.delayed(const Duration(seconds: 1), () {
-        setState(() {
-          _error = null; // Clear error message
-        });
-      });
-    }
+    });
+
+      }
+   
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text(
+        'No item added yet',
+        style: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+    );
+
+    if(_isLoading){
+      content = const Center(child: CircularProgressIndicator(),);
+    }
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
+    if(_error != null){
+      content = Center(
+      child: Text(
+        _error!,
+      ),
+    );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your grocery list'),
@@ -107,53 +172,7 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: FutureBuilder(
-          future: _loadedItems,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  snapshot.error.toString(),
-                ),
-              );
-            }
-            if (snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text(
-                  'No item added yet',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-              );
-            }
-
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (ctx, index) => Dismissible(
-                onDismissed: (direction) {
-                  _removeItem(snapshot.data![index]);
-                },
-                key: ValueKey(snapshot.data![index].id),
-                child: ListTile(
-                  title: Text(snapshot.data![index].name),
-                  leading: Container(
-                    width: 24,
-                    height: 24,
-                    color: snapshot.data![index].category.color,
-                  ),
-                  trailing: Text(
-                    snapshot.data![index].quantity.toString(),
-                  ),
-                ),
-              ),
-            );
-          }),
+      body: content,
     );
   }
 }
